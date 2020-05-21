b14cbcbf2156860ee824d4dd4f093259a116d850
LinkClinton <LinkClinton@outlook.com>
2020-05-21 11:11:27 +0800

    update camera.

--- rainbow/cameras/perspective_camera.cpp
+++ rainbow/cameras/perspective_camera.cpp
@@ -2,12 +2,21 @@
 
 rainbow::cameras::perspective_camera::perspective_camera(
 	const std::shared_ptr<cameras::film>& film,
-	const transform& transform,
 	const bound2& screen_window,
+	const transform& projective, 
+	const transform& transform) :
+	projective_camera(film, projective, transform, screen_window)
+{
+}
+
+rainbow::cameras::perspective_camera::perspective_camera(
+	const std::shared_ptr<cameras::film>& film,
+	const bound2& screen_window,
+	const transform& transform,
 	const real fov) :
 	projective_camera(film, perspective(fov,
 		static_cast<real>(film->resolution().x), 
-		static_cast<real>(film->resolution().y), mCoordinateSystem), transform, screen_window)
+		static_cast<real>(film->resolution().y)), transform, screen_window)
 {
 }
 
@@ -17,20 +26,8 @@ rainbow::cameras::perspective_camera::perspective_camera(
 	const real fov) :
 	projective_camera(film, perspective(fov, 
 		static_cast<real>(film->resolution().x), 
-		static_cast<real>(film->resolution().y), mCoordinateSystem), transform, 
+		static_cast<real>(film->resolution().y)), transform, 
 		bound2(vector2(-1), vector2(1)))
-{
-}
-
-rainbow::cameras::perspective_camera::perspective_camera(
-	const std::shared_ptr<cameras::film>& film,
-	const transform& transform, 
-	const coordinate_system& system, const real fov) :
-	projective_camera(film, perspective(fov,
-		static_cast<real>(film->resolution().x),
-		static_cast<real>(film->resolution().y), system), transform,
-		bound2(vector2(-1), vector2(1))),
-	mCoordinateSystem(system)
 {
 }
 
@@ -41,9 +38,9 @@ rainbow::ray rainbow::cameras::perspective_camera::generate_ray(const vector2& p
 	return mCameraToWorld(ray(normalize(target), vector3(0)));
 }
 
-rainbow::transform rainbow::cameras::perspective_camera::perspective(real fov, real width, real height, const coordinate_system& system)
+rainbow::transform rainbow::cameras::perspective_camera::perspective(real fov, real width, real height, bool right_hand)
 {
-	if (system == coordinate_system::right_hand)
+	if (right_hand == true)
 		return perspective_right_hand(fov, width, height);
 	else
 		return perspective_left_hand(fov, width, height);
--- rainbow/cameras/perspective_camera.hpp
+++ rainbow/cameras/perspective_camera.hpp
@@ -6,34 +6,28 @@ namespace rainbow {
 
 	namespace cameras {
 
-		enum class coordinate_system : uint32 {
-			right_hand = 0, left_hand = 1
-		};
-		
 		class perspective_camera final : public projective_camera {
 		public:
 			explicit perspective_camera(
 				const std::shared_ptr<cameras::film>& film,
-				const transform& transform,
 				const bound2& screen_window,
-				const real fov);
-
-			explicit perspective_camera(
-				const std::shared_ptr<cameras::film>& film,
 				const transform& transform,
 				const real fov);
 
 			explicit perspective_camera(
 				const std::shared_ptr<cameras::film>& film,
+				const bound2& screen_window,
+				const transform& projective,
+				const transform& transform);
+			
+			explicit perspective_camera(
+				const std::shared_ptr<cameras::film>& film,
 				const transform& transform,
-				const coordinate_system& system,
 				const real fov);
 			
 			ray generate_ray(const vector2& position) const noexcept override;
 		private:
-			static transform perspective(real fov, real width, real height, const coordinate_system& system);
-			
-			coordinate_system mCoordinateSystem = coordinate_system::right_hand;
+			static transform perspective(real fov, real width, real height, bool right_hand = true);
 		};
 		
 	}
